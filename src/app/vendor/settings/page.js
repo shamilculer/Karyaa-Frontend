e9516a00070009ba65e2e43bd53d7853aacdc233
shamilculer <shamilculer10@gmail.com>
2025-11-05 16:58:36 +0530

    new draft

--- src/app/vendor/settings/page.js
+++ src/app/vendor/settings/page.js
@@ -16,18 +16,18 @@ const VendorSettingsPage = async () => {
                 <div className='w-full flex-between max-lg:flex-col gap-10 lg:gap-48 pb-10 border-b border-gray-300'>
                     <div className='flex items-center max-lg:flex-col gap-4'>
                         <Avatar className="size-28" >
-                            <AvatarImage src={vendor.businessLogo} />
-                            <AvatarFallback >{getInitials(vendor.businessName) || "VNR"}</AvatarFallback>
+                            <AvatarImage src={vendor?.businessLogo || ''} alt={vendor?.businessName || 'Vendor'} />
+                            <AvatarFallback>{getInitials(vendor?.businessName || 'VNR')}</AvatarFallback>
                         </Avatar>
 
                         <div>
-                            <h3 className='max-lg:text-center'>{vendor.businessName || "Vendor"}</h3>
-                            <p className='!text-sm text-gray-500 max-lg:text-center'>{vendor.tagline || "Vendor Tagline"}</p>
+                            <h3 className='max-lg:text-center'>{vendor?.businessName || "Vendor"}</h3>
+                            <p className='!text-sm text-gray-500 max-lg:text-center'>{vendor?.tagline || "Vendor Tagline"}</p>
                         </div>
                     </div>
 
                     <div className='flex-center gap-4'>
-                       <Button asChild><Link href={`/vendors/${vendor.slug}`}><Eye className='w-5' /> Preview Profile</Link></Button>
+                       <Button asChild><Link href={`/vendors/${vendor?.slug || '#'}`}><Eye className='w-5' /> Preview Profile</Link></Button>
                         <Button asChild><Link href="settings/edit-profile"><UserPen className='w-5' /> Edit Profile</Link></Button>
                     </div>
                 </div>
